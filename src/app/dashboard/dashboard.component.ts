a675b585a913368f70d5c7e69c935bb11e6090f8
Mike Diamantopoulos <mdiamantopoulos@novibet.com>
2024-06-02 15:40:03 +0300

    changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  totalPoints = 20000
+  usedPoints = 5000;
 }
